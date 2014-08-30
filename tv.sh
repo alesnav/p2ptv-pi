dd1eabdecd33c1d0af55e858d6441cd38fc1c739
alesnav <a.navarro.montiel@gmail.com>
2014-08-30 20:06:56 +0200

    Añadida opción para ver la programación

--- tv.sh
+++ tv.sh
@@ -27,6 +27,8 @@ usage()
 	echo " -v			Muestra la versión."
 	echo " -s [0|1]		Apaga OMXPlayer y cierra la conexión P2P TV. 0: No iniciar XBMC. 1: Iniciar XBMC."
 	echo " -l			Lista de todos los canales preconfigurados."
+	echo " -p [n]			Muestra la programación de ArenaVision para el dia indicado."
+	echo "			[n] indica el día de la prohramación, siendo 0 el día actual, 1 el día siguiente, etc."
 	echo " -c [CANAL]		Indica el canal a cargar (ver formatos admitidos)."
 	echo " -o [0|1]		Apaga XBMC e inicia OMXPlayer. 0: Salida de video por defecto. 1: Salida por HDMI."
 	echo " -t [n]			Indica el tiempo en segundos a esperar para la carga del canal antes de iniciar OMXPlayer (15 por defecto)."
@@ -91,9 +93,14 @@ list_channels()
 	done
 }
 
+show_schedule()
+{
+	w3m -dump http://go.arenavision.in/p/agendaschedule.html -cols 500 | grep `date +%d/%m/%y -d "+$1 days"`
+}
+
 [[ $# -lt 1 ]] && usage && exit 1
 
-while getopts ":hVvt:s:lo:c:" OPTION
+while getopts ":hVvp:t:s:lo:c:" OPTION
 do
 	case "$OPTION" in
 		h)
@@ -108,6 +115,17 @@ do
 			VERBOSE=1
 			;;
 
+		p)
+			re='^[0-9]+$'
+			if ! [[ ${OPTARG} =~ ${re} ]] ; then
+				usage
+				exit 1
+			else
+				SCH_DAYS="${OPTARG}"
+				show_schedule ${SCH_DAYS}
+				exit 0
+			fi
+			;;
 		t)
 			re='^[0-9]+$'
 			if ! [[ ${OPTARG} =~ ${re} ]] ; then
